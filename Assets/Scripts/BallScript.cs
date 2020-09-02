30cfaa3e48d6bbc6432c35b3daab7aefac77f9c0
PashaahsaP <ppsairda@yandex.ru>
2020-09-02 20:03:23 +0700

    first commit

---
+++ Assets/Scripts/BallScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEditor.U2D;
+using UnityEngine;
+using UnityEngine.Assertions.Must;
+
+public class BallScript : MonoBehaviour
+{
+    public float speed = 10f;
+    Vector3 initialPosition;
+    
+    float halfwight;
+    float halfHeght;
+    float diametrBall;
+    Rigidbody2D ballRigidbody;
+    Vector2 velocity;
+    void Start()
+    {
+        initialPosition = transform.position;
+        halfHeght = Camera.main.orthographicSize;
+        halfwight = Camera.main.aspect * Camera.main.orthographicSize;
+        diametrBall = transform.localScale.x;
+        ballRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         
+              velocity = Vector2.up * speed;
+        
+      
+    }
+      void FixedUpdate()
+    {
+        ballRigidbody.position += velocity * Time.deltaTime;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿
+using Unity.Mathematics;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 7f;
+    float halfScreenWight;
+    float wightPlayer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        wightPlayer = transform.localScale.x;
+        halfScreenWight = Camera.main.aspect * Camera.main.orthographicSize - wightPlayer / 2f;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float inputX = Input.GetAxisRaw("Horizontal");
+        Vector2 velocity = Vector2.right * inputX * speed;
+        transform.Translate(velocity * Time.deltaTime);
+        if (transform.position.x > halfScreenWight)
+            transform.position = new Vector2(halfScreenWight, transform.position.y);
+        if (transform.position.x < -halfScreenWight)
+            transform.position = new Vector2(-halfScreenWight, transform.position.y);
+    }
+}
